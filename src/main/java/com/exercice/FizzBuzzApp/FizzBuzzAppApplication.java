abb1c3c5c028668a5c02670b25a1e1227bf36c1b
Aleix <aleixriba33@gmail.com>
2018-09-28 17:09:24 +0200

    Spring Boot added

---
+++ src/main/java/com/exercice/FizzBuzzApp/FizzBuzzAppApplication.java
@@ -0,0 +1,12 @@
+package com.exercice.FizzBuzzApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzBuzzAppApplication.class, args);
+	}
+}
