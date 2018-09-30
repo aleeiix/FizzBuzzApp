43a9bf9717f5f731e671cd31b8aaa649a3deef86
Aleix <aleixriba33@gmail.com>
2018-09-30 16:21:06 +0200

    added the entry point

--- src/main/java/com/exercice/FizzBuzzApp/controller/FizzBuzzController.java
+++ src/main/java/com/exercice/FizzBuzzApp/controller/FizzBuzzController.java
@@ -4,6 +4,8 @@ package com.exercice.FizzBuzzApp.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exercice.FizzBuzzApp.thread.WriteFileThread;
@@ -13,6 +15,22 @@ public class FizzBuzzController {
 	
 	@Value("${numberEnd}")
     private Integer numberEnd;
+	
+	@GetMapping("/maxNum")
+	Integer getMaxNum() {
+		return numberEnd;
+	}
+	
+	@PostMapping("/start")
+	Integer startGame(@RequestBody Integer numRandom) {
+		try {
+			Thread process = new WriteFileThread("processWriteFile",2,numberEnd);
+			process.start();
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		return 0;
+	}
 	
 	@GetMapping("/start")
 	Integer startGame() {
